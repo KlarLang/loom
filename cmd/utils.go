eb4f5da9773ce4f4ab286aef5045c790cd34a7e6
lucaass-d3v <lucas.workps@gmail.com>
2025-12-01 17:02:18 -0300

    version: Versão corrigida

--- cmd/utils.go
+++ cmd/utils.go
@@ -121,7 +121,7 @@ func (l Log) padCenter(text string, width int) string {
 
 func NewLog() Log {
 	return Log{
-		LoomVersion: "v0.7.0",
+		LoomVersion: "v0.7.2",
 		KlangVersion: "v0.1.10",
 		
 		RESET_COLOR:   "\033[0m",
